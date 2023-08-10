5c70abf270ed5ead75392589482aec0884413dea
PL LAB <PL LAB@PLLAB01>
2023-08-10 11:21:15 +0530

    First Version

---
+++ src/myCalculator1/addition.java
@@ -0,0 +1,8 @@
+package myCalculator1;
+
+public class addition {
+	public void sum(int x,int y) {
+	
+	}
+
+}
---
+++ src/myCalculator1/calculator.java
@@ -0,0 +1,15 @@
+package myCalculator1;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		addition a=new addition();
+		subtration s=new subtration();
+		a.sum(10,20);
+		s.sub(20,10);
+
+	}
+
+}
